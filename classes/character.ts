64bdb8e18595657451baded6d78fab68dfd33ba3
cerbin1 <gekarez@gmail.com>
2018-03-15 14:02:50 +0100

    Move fields level and experience to character.ts

--- classes/character.ts
+++ classes/character.ts
@@ -1,10 +1,30 @@
 class Character {
+    private _level: number;
+    private _experience: number;
     private _maxHP: number;
     private _actualHP: number;
 
     constructor() {
+        this._level = 0;
+        this._experience = 0;
         this._maxHP = 10;
         this._actualHP = 10;
+    }
+
+    get level(): number {
+        return this._level;
+    }
+
+    set level(value: number) {
+        this._level = value;
+    }
+
+    get experience(): number {
+        return this._experience;
+    }
+
+    set experience(value: number) {
+        this._experience = value;
     }
 
     get maxHP(): number {
--- classes/player.ts
+++ classes/player.ts
@@ -1,11 +1,6 @@
 class Player {
-    private _level: number;
-    private _experience: number;
-
     constructor(private _name: string) {
         this._name = _name;
-        this._level = 0;
-        this._experience = 0;
     }
 
     get getName(): string {
@@ -14,21 +9,5 @@ class Player {
 
     set name(value: string) {
         this._name = value;
-    }
-
-    get level(): number {
-        return this._level;
-    }
-
-    set level(value: number) {
-        this._level = value;
-    }
-
-    get experience(): number {
-        return this._experience;
-    }
-
-    set experience(value: number) {
-        this._experience = value;
     }
 }
