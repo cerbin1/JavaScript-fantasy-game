4ac56efa98e9ea623ec2d238b1631ba4429364ca
cerbin1 <gekarez@gmail.com>
2018-03-15 15:40:11 +0100

    Move content of file character.ts to player.ts

--- classes/player.ts
+++ classes/player.ts
@@ -1,6 +1,15 @@
 class Player {
+    private _level: number;
+    private _experience: number;
+    private _maxHP: number;
+    private _actualHP: number;
+
     constructor(private _name: string) {
         this._name = _name;
+        this._level = 0;
+        this._experience = 0;
+        this._maxHP = 10;
+        this._actualHP = 10;
     }
 
     get getName(): string {
@@ -9,5 +18,37 @@ class Player {
 
     set name(value: string) {
         this._name = value;
+    }
+
+    get level(): number {
+        return this._level;
+    }
+
+    set level(value: number) {
+        this._level = value;
+    }
+
+    get experience(): number {
+        return this._experience;
+    }
+
+    set experience(value: number) {
+        this._experience = value;
+    }
+
+    get maxHP(): number {
+        return this._maxHP;
+    }
+
+    set maxHP(value: number) {
+        this._maxHP = value;
+    }
+
+    get actualHP(): number {
+        return this._actualHP;
+    }
+
+    set actualHP(value: number) {
+        this._actualHP = value;
     }
 }
